70795b1e9c35304f0bf338d84ece5d992b086467
hfog2066 <hfog2000@yahoo.com>
2020-12-07 15:52:53 -0500

    new puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint
+# install puppet-lint, version 2.1.1
+
 package { 'puppet-lint':
-    ensure  => '2.1.1',
-    name    => 'puppet-lint'
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
